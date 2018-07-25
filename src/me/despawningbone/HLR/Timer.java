b7e1db71f2e018c62325a69a735168e1e7852508
despawningbone <laurence18lee@gmail.com>
2018-07-26 07:43:31 +0800

    Code cleanup

--- src/me/despawningbone/HLR/Timer.java
+++ src/me/despawningbone/HLR/Timer.java
@@ -4,9 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class Timer {
-	public static int taskid;
-    public void main(Player player) {
-        taskid = Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("HLR"), new Runnable() {
+    public static void cooldown(Player player) {
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("HLR"), new Runnable() {
             @Override
             public void run() {
             		HLRCommandMain.start.put(player, false);
